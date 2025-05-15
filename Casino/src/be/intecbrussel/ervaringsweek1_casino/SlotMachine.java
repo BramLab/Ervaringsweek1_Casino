05019a865a5306d6af9008efb79c329a809df141
oifjq <bram.labarque@gmail.com>
2025-05-15 12:42:59 +0200

    Level 2 SlotMachine.java

---
+++ Casino/src/be/intecbrussel/ervaringsweek1_casino/SlotMachine.java
@@ -0,0 +1,29 @@
+package be.intecbrussel.ervaringsweek1_casino;
+
+public class SlotMachine {
+
+    private int currentPayout;
+    private int odds;
+
+    public SlotMachine(){
+
+    }
+
+    public int getCurrentPayout() {
+        return currentPayout;
+    }
+
+    public void setCurrentPayout(int currentPayout) {this.currentPayout = currentPayout;}
+    public int getOdds() {return odds;}
+    public void setOdds(int odds) {this.odds = odds;}
+
+    private void whatOddsToGive(){
+        //
+    }
+
+    public int playTheSlots(int moneyPaid){
+        return 0;
+    }
+
+
+}
