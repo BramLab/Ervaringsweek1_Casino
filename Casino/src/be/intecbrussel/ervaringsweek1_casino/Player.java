98a409130b3521722cd578c1f20025dd8e51fa88
Tamara060890 <toma_tomova@hotmail.com>
2025-05-16 15:02:10 +0200

    Add files via upload

--- Casino/src/be/intecbrussel/ervaringsweek1_casino/Player.java
+++ Casino/src/be/intecbrussel/ervaringsweek1_casino/Player.java
@@ -1,5 +1,52 @@
-package be.intecbrussel.ervaringsweek1_casino;
-
+//Klasse die een speler voorstelt in het casino, met een naam en een saldo
 public class Player {
+    private String name;
+    private int money;
+    private int totalWinMoney;  // Totaal gewonnen geld
+    private int totalLostMoney; // Totaal verloren geld
 
+    //Constructor om een speler te maken met een naam en hoeveelheid geld
+    public Player(String name, int money) {
+        this.name = name;
+        this.money = money;
+        this.totalWinMoney = 0;
+        this.totalLostMoney = 0;
+    }
+
+    //Getter voor de naam van de persoon
+    public String getName() {
+        return name;
+    }
+
+    //Getter voor het huidige geld van de persoon
+    public int getMoney() {
+        return money;
+    }
+
+    // Methode om de hoeveelheid geld van de speler te verhogen
+    public void addMoney(int amount) {
+        money += amount;
+        totalWinMoney += amount; // Tel het gewonnen bedrag bij het totaal op
+    }
+
+    //Methode om de hoeveelheid geld van de speler te verminderen
+    public void loseMoney(int amount) {
+        money -= amount;
+        totalLostMoney += amount; // Tel het verloren bedrag bij het totaal op
+    }
+
+    //Methode om te controleren of de persoon genoeg geld heeft om te spelen
+    public boolean canPlay(){
+        return money > 0;
+    }
+    
+    // Methode om het totaal gewonnen geld weer te geven
+    public void showWinMoney() {
+        System.out.println(name + " heeft in totaal " + totalWinMoney + " euro gewonnen.");
+    }
+
+    // Methode om het totaal verloren geld weer te geven
+    public void showLostMoney() {
+        System.out.println(name + " heeft in totaal " + totalLostMoney + " euro verloren.");
+    }
 }
